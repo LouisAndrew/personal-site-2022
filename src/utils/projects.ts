6fc1c38f9f9bbbb7a248822385b6801d22f556c1
Louis Andrew <louisandrew3@gmail.com>
2022-10-09 20:29:19 +0200

    feat(projects): hide projects without description

---
+++ src/utils/projects.ts
@@ -0,0 +1,37 @@
+import type { RepositoryInfo } from "./types";
+
+type ProjectGroups = {
+  highlights: RepositoryInfo[];
+  additional: RepositoryInfo[];
+  others: RepositoryInfo[];
+};
+
+export const groupProjects = (projects: RepositoryInfo[]): ProjectGroups =>
+  projects.reduce(
+    (prev: ProjectGroups, current) => {
+      console.log(current);
+      if (current.highlight) {
+        return {
+          ...prev,
+          highlights: [...prev.highlights, current],
+        };
+      }
+
+      if (current.description) {
+        return {
+          ...prev,
+          additional: [...prev.additional, current],
+        };
+      }
+
+      return {
+        ...prev,
+        others: [...prev.others, current],
+      };
+    },
+    {
+      highlights: [],
+      additional: [],
+      others: [],
+    }
+  );
