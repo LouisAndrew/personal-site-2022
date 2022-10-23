9de9e0a23511e32ca2b782708b37fe922c16b239
Louis Andrew <louisandrew3@gmail.com>
2022-10-23 11:10:58 +0200

    fix(footer): fix broken styles on mobile (MOPS-0)

--- src/components/common/Footer.svelte
+++ src/components/common/Footer.svelte
@@ -1,24 +1,28 @@
 <footer
   class="flex justify-center bg-gray-100 dark:bg-zinc-700 text-gray-900 dark:text-slate-300 px-5 md:px-0"
 >
-  <div class="footer-content container grid py-2 text-xs">
-    <div>Made with ♡ by</div>
-    <a
-      class="block ml-1 duration-200 text-yellow-600 hover:text-yellow-500 dark:text-yellow-100 hover:dark:text-yellow-300"
-      href="https://github.com/LouisAndrew"
-      target="_blank"
-    >
-      @LouisAndrew
-    </a>
+  <div
+    class="footer-content container flex py-2 text-xs flex-col md:flex-row justify-between"
+  >
+    <div class="flex mb-1 md:mb-0">
+      <div>Made with ♡ by</div>
+      <a
+        class="block ml-1 duration-200 text-yellow-600 hover:text-yellow-500 dark:text-yellow-100 hover:dark:text-yellow-300"
+        href="https://github.com/LouisAndrew"
+        target="_blank"
+      >
+        @LouisAndrew
+      </a>
+    </div>
 
-    <div />
-
-    <div>Contact me at</div>
-    <a
-      class="block ml-1 text-yellow-600 hover:text-yellow-500 dark:text-yellow-100 hover:dark:text-yellow-300"
-      href="mailto:louisandrew3@gmail.com"
-    >
-      louisandrew3@gmail.com
-    </a>
+    <div class="flex">
+      <div>Contact me at</div>
+      <a
+        class="block ml-1 text-yellow-600 hover:text-yellow-500 dark:text-yellow-100 hover:dark:text-yellow-300"
+        href="mailto:louisandrew3@gmail.com"
+      >
+        louisandrew3@gmail.com
+      </a>
+    </div>
   </div>
 </footer>
